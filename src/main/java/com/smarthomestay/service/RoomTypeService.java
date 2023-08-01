a0d1c5457bcc578e112a6c8d3b189f04448634ca
muhamad rafli al rasyid <raflialrasyid99@gmail.com>
2023-08-01 14:18:18 +0700

    add project

--- src/main/java/com/smarthomestay/service/RoomTypeService.java
+++ src/main/java/com/smarthomestay/service/RoomTypeService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,7 +24,7 @@ public class RoomTypeService {
 
     public GeneralResponse<Object> findAll() {
         log.info("Start process get room type data");
-        List<RoomType> roomTypes;
+        List<RoomType> roomTypes = new ArrayList<>();
         try {
             roomTypes = roomTypeRepository.findAll();
         } catch (Exception e) {
